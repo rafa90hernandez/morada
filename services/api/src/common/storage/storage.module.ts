92fbc609380ab1dce5c09c4309bc9f4ca4a3a836
Rafael Hernandez <138315550+rafa90hernandez@users.noreply.github.com>
2026-08-09 20:52:40 +0100

    feat(storage): register private storage provider

--- services/api/src/common/storage/storage.module.ts
+++ services/api/src/common/storage/storage.module.ts
@@ -1,17 +1,26 @@
 import { Global, Module } from '@nestjs/common';
 
+import { LocalPrivateStorageService } from './local-private-storage.service';
 import { LocalStorageService } from './local-storage.service';
-import { STORAGE_SERVICE } from './storage.tokens';
+import {
+  PRIVATE_STORAGE_SERVICE,
+  STORAGE_SERVICE,
+} from './storage.tokens';
 
 @Global()
 @Module({
   providers: [
     LocalStorageService,
+    LocalPrivateStorageService,
     {
       provide: STORAGE_SERVICE,
       useExisting: LocalStorageService,
     },
+    {
+      provide: PRIVATE_STORAGE_SERVICE,
+      useExisting: LocalPrivateStorageService,
+    },
   ],
-  exports: [STORAGE_SERVICE],
+  exports: [STORAGE_SERVICE, PRIVATE_STORAGE_SERVICE],
 })
 export class StorageModule {}
