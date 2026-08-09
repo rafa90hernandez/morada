6f767a5b6bbe7b60801e4b93944c28411697355d
Rafael Hernandez <138315550+rafa90hernandez@users.noreply.github.com>
2026-08-09 20:54:48 +0100

    feat(verification): register identity submission flow

--- services/api/src/users/users.module.ts
+++ services/api/src/users/users.module.ts
@@ -1,12 +1,24 @@
 import { Module } from '@nestjs/common';
 
 import { ContactVerificationService } from './contact-verification.service';
+import { IdentityEvidenceImageProcessor } from './identity-evidence-image.processor';
+import { IdentityVerificationController } from './identity-verification.controller';
+import { IdentityVerificationSubmissionService } from './identity-verification-submission.service';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 
 @Module({
-  controllers: [UsersController],
-  providers: [UsersService, ContactVerificationService],
-  exports: [UsersService, ContactVerificationService],
+  controllers: [UsersController, IdentityVerificationController],
+  providers: [
+    UsersService,
+    ContactVerificationService,
+    IdentityEvidenceImageProcessor,
+    IdentityVerificationSubmissionService,
+  ],
+  exports: [
+    UsersService,
+    ContactVerificationService,
+    IdentityVerificationSubmissionService,
+  ],
 })
 export class UsersModule {}
